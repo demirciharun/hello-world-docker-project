28ad6b86f4aa8fd21d372868bb59033cd240f6ed
harun demirci <demirci@pop-os.localdomain>
2019-09-11 17:43:40 +0300

    first commit with Dockerfile and requirements.txt (settings.py changed allowed_hosts)

---
+++ hello/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+import textwrap
+from django.http import HttpResponse
+from django.views.generic.base import View
+
+class HomePageView(View):
+
+    def dispatch(request, *args, **kwargs):
+        response_text = textwrap.dedent('''\
+            <html>
+            <head>
+                <title>Hello World!</title>
+            </head>
+            <body>
+                <h1>Hello world</h1>
+                <p>Hello Docker!</p>
+            </body>
+            </html>
+        ''')
+        return HttpResponse(response_text)
